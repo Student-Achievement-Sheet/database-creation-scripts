26dca424838917e165efc85560ce7585733c9ba2
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-04-11 20:26:05 +0300

    Update 4_init_procedures.sql

--- ddl/4_init_procedures.sql
+++ ddl/4_init_procedures.sql
@@ -124,3 +124,84 @@ EXCEPTION
         ROLLBACK;
         DBMS_OUTPUT.PUT_LINE('Error: Unable to delete category.');
 END delete_achievements_category_by_id;
+
+# ========================================================================= CREATE ACHIEVEMENT ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE add_achievement (
+    p_user_id IN NUMBER,
+    p_category_id IN NUMBER,
+    p_description IN VARCHAR2,
+    p_proof_url IN VARCHAR2,
+    p_headline IN VARCHAR2,
+    p_visible_date IN VARCHAR2
+)
+AS
+BEGIN
+    INSERT INTO achievements (
+        achievements_category_id,
+        user_id,
+        description,
+        proof_url,
+        headline,
+        visible_date
+    ) VALUES (
+        p_category_id,
+        p_user_id,
+        p_description,
+        p_proof_url,
+        p_headline,
+        p_visible_date
+    );
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Achievement added successfully.');
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to add achievement.');
+END add_achievement;
+
+# ========================================================================= EDIT ACHIEVEMENT DESCRIPTION BY ACHIEVEMENT ID ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE update_achievement_description_by_id (
+    p_achievement_id IN NUMBER,
+    p_new_description IN VARCHAR
+)
+AS
+BEGIN
+    UPDATE achievements
+    SET description = p_new_description
+    WHERE id = p_achievement_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Achievement description updated successfully.');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Achievement with ID ' || p_achievement_id || ' not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to update achievement description.');
+END update_achievement_description_by_id;
+
+# ========================================================================= DELETE ACHIEVEMENT BY ID ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE delete_achievement_by_id (
+    p_achievement_id IN NUMBER
+)
+AS
+BEGIN
+    DELETE FROM achievements
+    WHERE id = p_achievement_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Achievement deleted successfully.');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Achievement with ID ' || p_achievement_id || ' not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to delete achievement.');
+END delete_achievement_by_id;
