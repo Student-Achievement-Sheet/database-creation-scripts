1387a0df417bc8ed26daf8aa224729b77bdc714b
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-04-11 20:26:47 +0300

    Update 5_init_test_data.sql

--- ddl/5_init_test_data.sql
+++ ddl/5_init_test_data.sql
@@ -3,3 +3,13 @@ EXEC ADD_USER('Alice Johnson', 'http://example.com/proof_2', '1990-05-25', 'http
 EXEC ADD_USER('Michael Brown', 'http://example.com/proof_3', '1982-11-10', 'http://example.com/photo_3', 'Group_3');
 EXEC ADD_USER('Emily Davis', 'http://example.com/proof_4', '1995-03-30', 'http://example.com/photo_4', 'Group_4');
 EXEC ADD_USER('Daniel Wilson', 'http://example.com/proof_5', '1988-07-20', 'http://example.com/photo_5', 'Group_5');
+
+EXEC
+    add_achievement(
+        p_user_id => 4,
+        p_category_id => 1,
+        p_description => 'Description of the achievement',
+        p_proof_url => 'http://example.com/proof',
+        p_headline => 'Headline of the achievement',
+        p_visible_date => '2024-04-12'
+    );
