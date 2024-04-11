6133f5b9299f4fe55f5f6bda5e0d7a2c3bdb41eb
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-04-11 19:33:27 +0300

    Update 4_init_procedures.sql

--- ddl/4_init_procedures.sql
+++ ddl/4_init_procedures.sql
@@ -1,3 +1,5 @@
+# ========================================================================= ADD USER ========================================================================= #
+
 CREATE OR REPLACE PROCEDURE add_user (
    full_name VARCHAR,
    proof_url VARCHAR,
@@ -14,4 +16,48 @@ BEGIN
 EXCEPTION
     WHEN OTHERS THEN
         ROLLBACK;
+END;
+
+# ========================================================================= DELETE USER ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE delete_user (
+    p_id IN NUMBER
+)
+AS
+BEGIN
+    DELETE FROM users
+    WHERE id = p_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('User deleted successfully.');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('User with ID ' || p_id || ' not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to delete user.');
+END;
+
+# ========================================================================= UPDATE USER FULLNAMEa ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE update_full_name_by_user_id (
+    p_id IN NUMBER,
+    p_new_full_name IN VARCHAR2
+)
+AS
+BEGIN
+    UPDATE users
+    SET full_name = p_new_full_name
+    WHERE id = p_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Full name updated successfully.');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('User with ID ' || p_id || ' not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to update full name.');
 END;
