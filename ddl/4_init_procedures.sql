681e4ae481e4343ba94f18ef7be0ecfe34782c29
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-04-11 20:09:48 +0300

    Update 4_init_procedures.sql

--- ddl/4_init_procedures.sql
+++ ddl/4_init_procedures.sql
@@ -61,3 +61,66 @@ EXCEPTION
         ROLLBACK;
         DBMS_OUTPUT.PUT_LINE('Error: Unable to update full name.');
 END;
+
+# ========================================================================= CREATE ACHIEVEMENT CATEGORY ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE insert_into_achievements_category (
+    p_category_name IN VARCHAR
+)
+AS
+BEGIN
+    INSERT INTO achievements_category (category_name)
+    VALUES (p_category_name);
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Data inserted into achievements_category successfully.');
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to insert data into achievements_category.');
+END insert_into_achievements_category;
+   
+# ========================================================================= UPDATE ACHIEVEMENT CATEGORY NAME BY ID ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE update_category_name_by_achievement_id (
+    p_category_id IN NUMBER,
+    p_new_category_name IN VARCHAR
+)
+AS
+BEGIN
+    UPDATE achievements_category
+    SET category_name = p_new_category_name
+    WHERE id = p_category_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Category name updated successfully.');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Category with ID ' || p_category_id || ' not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to update category name.');
+END update_category_name_by_achievement_id;
+   
+# ========================================================================= DELETE ACHIEVEMENT CATEGORY BY ID ========================================================================= #
+
+CREATE OR REPLACE PROCEDURE delete_achievements_category_by_id (
+    p_category_id IN NUMBER
+)
+AS
+BEGIN
+    DELETE FROM achievements_category
+    WHERE id = p_category_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Category deleted successfully.');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Category with ID ' || p_category_id || ' not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: Unable to delete category.');
+END delete_achievements_category_by_id;
