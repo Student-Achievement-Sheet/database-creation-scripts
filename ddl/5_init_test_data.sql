b0fd54f37c28db032a1698e81fab67510ec7ebf1
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-04-11 19:27:21 +0300

    Create 5_init_test_data.sql

---
+++ ddl/5_init_test_data.sql
@@ -0,0 +1,5 @@
+EXEC ADD_USER('John Smith', 'http://example.com/proof_1', '1985-09-15', 'http://example.com/photo_1', 'Group_1');
+EXEC ADD_USER('Alice Johnson', 'http://example.com/proof_2', '1990-05-25', 'http://example.com/photo_2', 'Group_2');
+EXEC ADD_USER('Michael Brown', 'http://example.com/proof_3', '1982-11-10', 'http://example.com/photo_3', 'Group_3');
+EXEC ADD_USER('Emily Davis', 'http://example.com/proof_4', '1995-03-30', 'http://example.com/photo_4', 'Group_4');
+EXEC ADD_USER('Daniel Wilson', 'http://example.com/proof_5', '1988-07-20', 'http://example.com/photo_5', 'Group_5');
