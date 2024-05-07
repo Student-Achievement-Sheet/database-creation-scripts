88035c53e3195a8015ef8d1dca8a2f39155d7adf
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-05-07 16:12:24 +0300

    Update 2_task.sql

--- selects/2_task.sql
+++ selects/2_task.sql
@@ -1,5 +1,27 @@
+Выести самого успешного вывести и самую успешную его категорию
+
+# Самый успешный
 SELECT user_id, COUNT(*) AS row_count
 FROM achievements
 GROUP BY user_id
 ORDER BY row_count DESC
 LIMIT 1;
+
+# Самая успешная категория самого успешного студента
+SELECT category_name FROM (
+  SELECT achievement_category_id, COUNT(*) AS achievement_category_id_сount FROM achievements
+  INNER JOIN 
+    (SELECT user_id, COUNT(*) AS row_count
+    FROM achievements
+    GROUP BY user_id
+    ORDER BY row_count DESC
+    LIMIT 1) 
+    AS most_successful_user 
+  ON most_successful_user.user_id == achievements.user_id
+  GROUP BY achievement_category_id
+  ORDER BY achievement_category_id_сount DESС
+  LIMIT 1) 
+  AS most_successful_achievement_category
+INNER JOIN achievements_category
+ON achievements_category.id == achievement_category_id;
+
