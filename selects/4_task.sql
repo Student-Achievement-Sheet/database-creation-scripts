c939112b508143773e64a7e50ccf57d6af512ad2
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-05-07 16:16:29 +0300

    Update 4_task.sql

--- selects/4_task.sql
+++ selects/4_task.sql
@@ -1,5 +1,8 @@
-SELECT achievements_category_id, COUNT(*) AS row_count
+# Вывести самую непопулярную категорию
+
+SELECT achievements_category.category_name FROM (SELECT achievements_category_id, COUNT(*) AS row_count
 FROM achievements
 GROUP BY achievements_category_id
 ORDER BY row_count ASC
-LIMIT 1;
+LIMIT 1) as most_unpopular_category 
+INNER JOIN achievements_category ON achievements_category.id = achievements_category_id;
