4a2ced73714f1f1b74be3b26b3e4a0ee7566f315
Matster01 <163631824+Matster01@users.noreply.github.com>
2024-05-07 15:55:18 +0300

    Update 1_task.sql

--- selects/1_task.sql
+++ selects/1_task.sql
@@ -1,4 +1,6 @@
+## Для одного пользователя вывести категории, которые у него не заполнены
+
 SELECT users.id, users.full_name
 FROM users
 LEFT JOIN achievements ON users.id=achievements.user_id
-WHERE achievements.user_id is NULL;
+WHERE users.id == 1 AND achievements.user_id is NULL;
